294abe6074f07fa62a4450edf0a264b7ed1f6be2
찌원 남 <skawldnjs125@naver.com>
2021-10-19 17:59:50 +0900

    초기 커밋.

---
+++ WPF_Tutorial/Control/TextBlock.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Navigation;
+
+namespace WPF_Tutorial.Control
+{
+    /// <summary>
+    /// TextBlock.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class TextBlock : Window
+    {
+        public TextBlock()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            // 하이퍼 링크는 WPF page와 page 사이를 이동하는데에도 사용된다.
+            // 그럴 때는 이런 Process클래스 사용은 필요없다.
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+        }
+    }
+}
