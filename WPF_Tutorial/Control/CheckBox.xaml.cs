05859d37266a7be804db0cd9f02dcd6c3a0609b7
남지원 <51081373+Bantree96@users.noreply.github.com>
2021-10-24 14:22:27 +0900

    1. CheckBox 2. PasswordBox 3. RadioButton 4. ToolTips

--- WPF_Tutorial/Control/CheckBox.xaml.cs
+++ WPF_Tutorial/Control/CheckBox.xaml.cs
@@ -23,5 +23,23 @@ namespace WPF_Tutorial.Control
         {
             InitializeComponent();
         }
+
+        // 전체 눌렀을 때
+        private void cbAllFeatures_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            bool newVal = (cbAllFeatures.IsChecked == true);
+            cbFeatureAbc.IsChecked = newVal;
+            cbFeatureXyz.IsChecked = newVal;
+            cbFeatureWww.IsChecked = newVal;
+        }
+
+        private void cbFeature_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            cbAllFeatures.IsChecked = null;
+            if ((cbFeatureAbc.IsChecked == true) && (cbFeatureXyz.IsChecked == true) && (cbFeatureWww.IsChecked == true))
+                cbAllFeatures.IsChecked = true;
+            if ((cbFeatureAbc.IsChecked == false) && (cbFeatureXyz.IsChecked == false) && (cbFeatureWww.IsChecked == false))
+                cbAllFeatures.IsChecked = false;
+        }
     }
 }
