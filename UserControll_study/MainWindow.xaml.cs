d3154181600db4fd00750c02e85a6a8f054113a2
남지원 <jwnam@crevis.co.kr>
2022-06-22 11:43:15 +0900

    ICommand를 통해 Page전환하기

---
+++ UserControll_study/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace UserControll_study
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainViewModel mainViewModel;
+        public MainWindow()
+        {
+            mainViewModel = new MainViewModel();
+            InitializeComponent();
+            this.DataContext = mainViewModel;
+        }
+    }
+}
---
+++ UserControll_study/Pages/MainPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace UserControll_study.Page
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        #region Binding
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
