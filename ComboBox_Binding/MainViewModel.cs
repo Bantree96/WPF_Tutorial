7a28b35c2723f4b9f2476d01b3825c09aaeceb70
남지원 <jwnam@crevis.co.kr>
2022-06-28 08:52:43 +0900

    ComboBox 바인딩 예제

---
+++ ComboBox_Binding/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ComboBox_Binding
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        #region Binding
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                // View 
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        #region Field
+        private string _txt;
+
+        #endregion
+
+        #region Property
+        public string Txt { get { return _txt; } set { _txt = value; OnPropertyChanged(nameof(Txt)); } }
+        public Array MyEnumArray
+        {
+            get { return Enum.GetValues(typeof(MyEnum)); }
+        }
+
+        public enum MyEnum
+        {
+            a,b,c,
+        } 
+        #endregion
+
+        #region Constructor
+        public MainViewModel()
+        {
+            Txt = "abc";
+        }
+        #endregion
+    }
+}
