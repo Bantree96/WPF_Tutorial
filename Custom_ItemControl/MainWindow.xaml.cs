013dcc379847403c0c267a64c28af66280c67365
남지원 <jwnam@crevis.co.kr>
2022-07-27 15:43:52 +0900

    ItemControl 공부

---
+++ Custom_ItemControl/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Custom_ItemControl
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            MainWindowViewModel mainWindowViewModel = new MainWindowViewModel();
+            InitializeComponent();
+            this.DataContext = mainWindowViewModel;
+        }
+    }
+}
---
+++ Custom_ItemControl/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Media.Imaging;
+
+namespace Custom_ItemControl
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region Binding
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                // View 
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        private ObservableCollection<Item> _items;
+        public ObservableCollection<Item> Items { get { return _items; } set { _items = value; OnPropertyChanged(nameof(Items)); } }
+
+        public MainWindowViewModel()
+        {
+            Items = new ObservableCollection<Item>();
+
+            Item a = new Item();
+            Items.Add(a);
+            Items.Add(a);
+            Items.Add(a);
+        }
+    }
+
+    public class Item 
+    {
+
+        public BitmapImage Image { get; set; }
+        public int Score { get; set; } = 0;
+        public string Text { get; set; } = "abc";
+    }
+}
