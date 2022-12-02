cebf4646bb1038668f064dd1480788fdf8dbccc6
남지원 <jwnam@crevis.co.kr>
2022-12-02 17:33:46 +0900

    Focus

---
+++ Focus_Nobody/FocusBehavior.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Focus_Nobody
+{
+    class FocusBehavior : Behavior<TextBoxBase>
+    {
+        public static readonly DependencyProperty IsControlFocusProperty = DependencyProperty.Register(
+            nameof(IsControlFocus),
+            typeof(bool),
+            typeof(FocusBehavior),
+            new PropertyMetadata(false, propertyChangedCallback: IsControlFocusPropertyChanged)
+        );
+
+        public bool IsControlFocus
+        {
+            get;
+            set;
+        }
+
+        private static void IsControlFocusPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is FocusBehavior behavior)
+            {
+                behavior.AssociatedObject.Focus();
+                behavior.AssociatedObject.SelectAll();
+            }
+        }
+    }
+}
---
+++ Focus_Nobody/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Focus_Nobody
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            // Invoke = UI스레드에 동기식으로 작업목록 추가
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private bool _isUserNameFocus;
+        public bool IsUserNameFocus { get => _isUserNameFocus; set { _isUserNameFocus = value; OnPropertyChanged(nameof(IsUserNameFocus)); } }
+        public Command BtnClick { get; set; }
+        public MainWindowViewModel()
+        {
+            BtnClick = new Command(OnBtnClick, CanExecute_func);
+        }
+        private bool CanExecute_func(object obj)
+        {
+            return true;
+        }
+
+        private void OnBtnClick(object obj)
+        {
+            IsUserNameFocus = true;
+        }
+    }
+}
