52c6f699f5a0d79979d6e7e90915aa26c374fb23
남지원 <jwnam@crevis.co.kr>
2022-12-02 12:26:43 +0900

    MVVM으로 포커스 하는 방법

---
+++ MVVM_Focus/IReqeustFocus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Focus
+{
+    public interface IReqeustFocus
+    {
+        event EventHandler<FocusRequestedEventArgs> FocusReqested;
+    }
+
+    public class FocusRequestedEventArgs : EventArgs
+    {
+        public string PropertyName { get; private set; }
+
+        public FocusRequestedEventArgs(string propertyName)
+        {
+            PropertyName = propertyName;
+        }
+
+    }
+}
---
+++ MVVM_Focus/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Focus
+{
+    class MainWindowViewModel : INotifyPropertyChanged, IReqeustFocus
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler<FocusRequestedEventArgs> FocusReqested;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            // Invoke = UI스레드에 동기식으로 작업목록 추가
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void OnFocusReqeusted(string propertyName)
+        {
+            FocusReqested?.Invoke(this, new FocusRequestedEventArgs(propertyName));
+        }
+
+        private string _id;
+        public string ID { get => _id; set { _id = value; OnPropertyChanged(nameof(ID)); } }
+
+        public Command BtnClick { get; set; }
+        public MainWindowViewModel()
+        {
+            BtnClick = new Command(OnBtnClick, CanExecute_func);
+        }
+
+        private void OnBtnClick(object obj)
+        {
+            OnFocusReqeusted(nameof(ID));
+        }
+
+        private bool CanExecute_func(object obj)
+        {
+            return true;
+        }
+
+
+    }
+}
