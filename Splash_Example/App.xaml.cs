dc933b70b00595643e52d078eae8ff83104032c0
찌원 남 <skawldnjs125@naver.com>
2025-03-23 20:25:21 +0900

    솔루션정리 및 Splash추가

---
+++ Splash_Example/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Splash_Example.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Splash_Example
+{
+	/// <summary>
+	/// App.xaml에 대한 상호 작용 논리
+	/// </summary>
+	public partial class App : Application
+	{
+		private static string _splashWindowPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\splashwindow.bmp";
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			MainWindowViewModel viewModel = new MainWindowViewModel();
+
+			MainWindow mainWindow =	new MainWindow(viewModel);
+
+			SplashScreen splashScreen = new SplashScreen(_splashWindowPath);
+			splashScreen.Show(false);
+
+			mainWindow.Show();
+        }
+
+    }
+}
---
+++ Splash_Example/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splash_Example.ViewModels
+{
+	public class MainWindowViewModel
+	{
+		public string Title { get; set; } = "Hello";
+
+		public MainWindowViewModel()
+		{
+				
+		}
+	}
+}
