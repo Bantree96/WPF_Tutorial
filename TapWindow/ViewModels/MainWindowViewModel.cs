fe90e1a04aa6615f31c238b0df2333c393a312aa
남지원 <skawl@JIWON-LAPTOP>
2025-04-13 17:36:11 +0900

    세팅창만들려다 DialogService 구현중

---
+++ TapWindow/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Utils.Core.Binding;
+using Utils.Core.Buttons;
+
+namespace TapWindow.ViewModels
+{
+    public class MainWindowViewModel : NotifyBase
+    {
+        public string Title { get; set; } = "TapWindow";
+        public ICommand DialogCommand => new Command(OnShowDialog);
+
+        private void OnShowDialog(object obj)
+        {
+            MessageBox.Show("Hello, World!");
+        }
+
+        public MainWindowViewModel()
+        {
+            
+        }
+    }
+}
---
+++ Utils.Core/Buttons/Command.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Utils.Core.Buttons
+{
+    public class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        // 실행 함수 로직을 저장
+        Action<object> _execute;
+
+        // 지금 실행 가능한지 판단하는 조건
+        Func<object, bool> _canExecute;
+        public Command(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        // 보통 UI에서 IsEnabled 속성에 영향을 줌
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+                return true;
+            return _canExecute(parameter);
+        }
+
+        // 커맨드를 실행 하는 부분
+        public void Execute(object parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                _execute(parameter);
+            }
+        }
+
+        // 보통 버튼 상태를 갱신할 때 사용
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
